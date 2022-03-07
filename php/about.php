d3e3a6273821181826fa7b7194d7e227e155eed9
Sumaiya47 <65812741+Sumaiya47@users.noreply.github.com>
2022-03-07 19:18:34 +0600

    3rd commit

---
+++ php/about.php
@@ -0,0 +1,70 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>About Us</title>
+  <link rel="stylesheet" href="css/all.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+    integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/fontawesome.min.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="../css/about.css?v=<?php echo time(); ?>">
+  <link rel="stylesheet" href="css/animate.min.css">
+</head>
+
+<body>
+         
+<header>
+    <nav>
+        <div class="row clearfix">
+            <div class="left">
+                <h4 class="change1">Online Library</h4>
+            </div>
+            <div class="right">
+                <ul class="main-nav animate__animated animate_slideInDown">
+                    <li><a href="index.html">HOME</a></li>
+                    <li><a href="about.html">ABOUT US</a></li>
+                    <li><a href="#">SERVICES</a></li>
+                    <li><a href="php/book.php">BOOKS</a></li>
+                    <li><a href="#">FEEDBACK</a></li>
+                    <li><a href="template/contact.html">CONTACT US</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+          <div class="about-section bg-transparent">
+              <div class="inner-container">
+                <h1>About</h1>
+                <div class="about">
+                  <div class="myimg">
+                      <img class="me" src="../image/Online.png" alt="">
+                  </div>
+                <p class="text">
+                    Online Library Management System is an Automated Library System that handles the various functions of the library. It provides a complete solution to the library management software. 
+                    Library Management System helps the students in better learning by providing quick access to the library system. With Advance Library Software they can easily find books, catalogs, magazines of their interest. The library is a great place to study and make notes which ultimately help in scoring a good score.
+
+
+
+                </p>
+
+              </div>
+              </div>
+          </div>
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</header>
+</body>
+
+</html>
