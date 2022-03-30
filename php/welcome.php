e67a2ef7ca4eb7a289b0bf8244eaf47826e07295
EvaAfrin <afrineva28@gmail.com>
2022-03-30 23:43:02 +0600

    eva00

--- php/welcome.php
+++ php/welcome.php
@@ -29,15 +29,14 @@ session_start();
                       
 
                         <li><a href="../index.html">HOME</a></li>
-                        <li><a href="../template/about.html">ABOUT US</a></li>
+                        
                         
 
-                        <li><a href="../template/services.html">SERVICES</a></li>
                         <li><a href="../php/book.php">BOOKS</a></li>
                         <li><a href="../php/feedback.php">FEEDBACK</a></li>
-                        <li><a href="contact.html">CONTACT US</a></li>
-                        <li> <a href=""><?php echo  $_SESSION["user_name"];?></a></li>
-                        <li><a href="../php/indexlog.php"><img src="../image/log.png" alt=""></a></li>
+                       
+                        <li> <a href="../php/indexlog.php"><?php echo  $_SESSION["user_name"];?></a></li>
+                        
                     </ul>
                 </div>
             </div>
